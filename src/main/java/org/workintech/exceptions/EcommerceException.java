2430cfd590cfbf156b4cca3306f41847898c2c89
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-07 00:20:29 +0300

    created exception handler

---
+++ src/main/java/org/workintech/exceptions/EcommerceException.java
@@ -0,0 +1,13 @@
+package org.workintech.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+@Getter
+public class EcommerceException extends RuntimeException{
+    private HttpStatus httpStatus;
+
+    public EcommerceException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
---
+++ src/main/java/org/workintech/exceptions/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package org.workintech.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ExceptionResponse {
+    private String message;
+}
