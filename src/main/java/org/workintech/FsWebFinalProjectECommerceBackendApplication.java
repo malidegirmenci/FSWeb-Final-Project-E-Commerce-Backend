8034450c546a97235b88a5baff6ba1d437ef3afa
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-06 18:34:29 +0300

    init commit

---
+++ src/main/java/org/workintech/FsWebFinalProjectECommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.workintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsWebFinalProjectECommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsWebFinalProjectECommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/workintech/FsWebFinalProjectECommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsWebFinalProjectECommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
