0e082af89022ee656e94a193944a0e7afbf9ba2c
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-08 01:49:31 +0300

    changed images type

--- src/main/java/org/workintech/dto/ProductResponse.java
+++ src/main/java/org/workintech/dto/ProductResponse.java
@@ -1,8 +1,6 @@
 package org.workintech.dto;
 
-import org.workintech.entity.ImagesObj;
-
 import java.util.List;
 
-public record ProductResponse(Long id, String name, String description, List<ImagesObj> images, Double price, Double rating, Integer stock, Integer sellCount, Long category_id, Long store_id) {
+public record ProductResponse(Long id, String name, String description, List<ImagesObjResponse> images, Double price, Double rating, Integer stock, Integer sellCount, Long category_id, Long store_id) {
 }
