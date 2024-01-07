072dcebd24472297979ce38e5f9fdeb09f466e16
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-07 05:17:17 +0300

    created imagesobj entity

---
+++ src/main/java/org/workintech/entity/ImagesObj.java
@@ -0,0 +1,29 @@
+package org.workintech.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "images", schema = "ecommerce")
+public class ImagesObj {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "index")
+    private int index;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
