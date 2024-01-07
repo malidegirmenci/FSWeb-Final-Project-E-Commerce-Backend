c42665ffb055da23276f8b2fb1be3df41375ea03
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-07 05:17:02 +0300

    created imagesobj repository

---
+++ src/main/java/org/workintech/repository/ImagesObjRepository.java
@@ -0,0 +1,7 @@
+package org.workintech.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.workintech.entity.ImagesObj;
+
+public interface ImagesObjRepository extends JpaRepository<ImagesObj,Long> {
+}
