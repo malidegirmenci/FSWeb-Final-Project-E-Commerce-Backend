b745e8dcb241516e0c07ac2255c4e8d60f02e19a
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-07 05:17:10 +0300

    created imagesobj controller

---
+++ src/main/java/org/workintech/controller/ImagesObjController.java
@@ -0,0 +1,48 @@
+package org.workintech.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.workintech.entity.ImagesObj;
+import org.workintech.entity.Product;
+import org.workintech.service.ImagesObjService;
+import org.workintech.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/products")
+public class ImagesObjController {
+    private ImagesObjService imagesObjService;
+    private ProductService productService;
+    private static final String GET_ALL_PRODUCTS = "https://workintech-fe-ecommerce.onrender.com/products";
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @PostMapping("/all/images")
+    public List<ImagesObj> saveAll() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<JsonNode> responses = restTemplate.getForEntity(GET_ALL_PRODUCTS, JsonNode.class);
+        List<ImagesObj> imagesObjList = new ArrayList<>();
+        for(JsonNode node : Objects.requireNonNull(responses.getBody().get("products"))){
+            JsonNode imagesNode = node.get("images") != null ? node.get("images") : null;
+            Long productId = node.get("id") != null ? node.get("id").asLong() : null;
+            assert imagesNode != null;
+            for(JsonNode subNode : imagesNode){
+                ImagesObj imagesObj = new ImagesObj();
+                imagesObj.setUrl(subNode.get("url").asText());
+                imagesObj.setIndex(subNode.get("index").asInt());
+                imagesObj.setProduct(productService.finById(productId));
+                imagesObjList.add(imagesObjService.save(imagesObj));
+            }
+        }
+        return imagesObjList;
+    }
+}
