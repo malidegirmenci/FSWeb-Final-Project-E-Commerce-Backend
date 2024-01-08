c8d607a3c6d4ca7bc55c20ba02249b901ed366e7
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-08 11:34:03 +0300

    created register controller

---
+++ src/main/java/org/workintech/controller/user/RegisterController.java
@@ -0,0 +1,24 @@
+package org.workintech.controller.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.workintech.converter.DtoConverter;
+import org.workintech.dto.user.RegisterUserRequest;
+import org.workintech.dto.user.RegisterUserResponse;
+import org.workintech.service.user.AuthenticationService;
+@AllArgsConstructor
+@RestController
+@RequestMapping("/signup")
+public class RegisterController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping
+    public RegisterUserResponse register(@RequestBody RegisterUserRequest registerUserRequest){
+        authenticationService
+                .register(registerUserRequest.name(), registerUserRequest.email(), registerUserRequest.password(), registerUserRequest.roleId());
+        return DtoConverter.convertToRegisterUserResponse("The user has been registered");
+    }
+}
