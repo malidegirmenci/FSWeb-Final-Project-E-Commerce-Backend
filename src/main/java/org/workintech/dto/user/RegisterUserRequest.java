185429c559be5fb1a455ffaeb8f8b8ad815cbe5e
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-09 20:43:38 +0300

    changed validation for role id

--- src/main/java/org/workintech/dto/user/RegisterUserRequest.java
+++ src/main/java/org/workintech/dto/user/RegisterUserRequest.java
@@ -1,10 +1,11 @@
 package org.workintech.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 public record RegisterUserRequest(
         @NotBlank(message = "Name must not be null, empty or blank")String name,
         @NotBlank(message = "Email must not be null, empty or blank")String email,
         @NotBlank(message = "Password must not be null, empty or blank")String password,
-        @NotBlank(message = "Role id must not be null, empty or blank")Long role_id) {
+        @Positive Long role_id) {
 }
