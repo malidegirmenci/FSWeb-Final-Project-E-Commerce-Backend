1e7b8cf2ab4e536a84ef68bff4763925f357b20d
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-09 10:03:32 +0300

    changed dummy token body

--- src/main/java/org/workintech/utils/Helper.java
+++ src/main/java/org/workintech/utils/Helper.java
@@ -1,9 +1,14 @@
 package org.workintech.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class Helper {
-    public static String generateDummyToken(){
-        return UUID.randomUUID().toString();
+    public static String generateDummyToken(String username){
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = currentDate.format(formatter);
+        return username+formattedDate;
     }
 }
