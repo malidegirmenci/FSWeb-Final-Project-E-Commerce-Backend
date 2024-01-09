4b9c02c2d1373d2318683b6a90e712f7e038c9d8
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-09 11:21:55 +0300

    added validation

--- src/main/java/org/workintech/entity/Store.java
+++ src/main/java/org/workintech/entity/Store.java
@@ -1,6 +1,8 @@
 package org.workintech.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,15 +20,23 @@ public class Store {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank(message = "Name must not be null, empty or blank")
+    @Size(min = 3,max = 35,message = "Name must not be less than 3 and greater than 35 characters.")
     @Column(name = "name")
     private String name;
 
+    @NotBlank(message = "Phone must not be null, empty or blank")
+    @Size(min = 10,max = 20,message = "Phone must not be less than 10 and greater than 20 characters.")
     @Column(name = "phone")
     private String phone;
 
+    @NotBlank(message = "Tax number must not be null, empty or blank")
+    @Size(min = 3,max = 50,message = "Tax number must not be less than 3 and greater than 50 characters.")
     @Column(name = "tax_number")
     private String taxNumber;
 
+    @NotBlank(message = "Iban must not be null, empty or blank")
+    @Size(min = 3,max = 50,message = "Iban must not be less than 3 and greater than 50 characters.")
     @Column(name = "iban")
     private String iban;
 
--- src/main/java/org/workintech/entity/user/Role.java
+++ src/main/java/org/workintech/entity/user/Role.java
@@ -1,6 +1,8 @@
 package org.workintech.entity.user;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +19,13 @@ public class Role implements GrantedAuthority {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank(message = "Name must not be null, empty or blank")
+    @Size(min = 1,max = 25,message = "Name must not be less than 3 and greater than 25 characters.")
     @Column(name = "name")
     private String name;
 
+    @NotBlank(message = "Code must not be null, empty or blank")
+    @Size(min = 2,max = 25,message = "Code must not be less than 2 and greater than 25 characters.")
     @Column(name = "code")
     private String code;
 
