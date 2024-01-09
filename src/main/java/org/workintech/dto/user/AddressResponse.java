53ab11e70aac7dea1379e66a7c12ae20230ecf16
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-09 10:06:16 +0300

    created Address response

---
+++ src/main/java/org/workintech/dto/user/AddressResponse.java
@@ -0,0 +1,4 @@
+package org.workintech.dto.user;
+
+public record AddressResponse(String addressTitle, String name, String surname , String phone, String city, String district, String neighborhood, String addressDetail) {
+}
