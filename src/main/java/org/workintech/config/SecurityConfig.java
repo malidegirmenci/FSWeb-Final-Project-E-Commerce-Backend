70ab2b26582b5b1e606aaddf5c7ca290b1f18a36
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-18 08:14:33 +0300

    added token configs

--- src/main/java/org/workintech/config/SecurityConfig.java
+++ src/main/java/org/workintech/config/SecurityConfig.java
@@ -1,5 +1,13 @@
 package org.workintech.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -9,18 +17,55 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.workintech.utils.RsaKeyProperty;
 
 import java.util.Arrays;
+import java.util.List;
 
+@Data
 @Configuration
 public class SecurityConfig {
+    private final RsaKeyProperty keys;
+    @Autowired
+    public SecurityConfig(RsaKeyProperty keys) {
+        this.keys = keys;
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+    }
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jks);
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtConverter;
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -35,7 +80,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -45,24 +90,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors().configurationSource(corsConfigurationSource());
-        return httpSecurity.csrf(csrf -> csrf.disable())
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(HttpMethod.GET,"/products/**").permitAll();
-                    auth.requestMatchers(HttpMethod.POST, "/product/**").hasAnyAuthority("admin","store");
-                    auth.requestMatchers(HttpMethod.PUT, "/product/**").hasAnyAuthority("admin","store");
-                    auth.requestMatchers(HttpMethod.DELETE, "/product/**").hasAnyAuthority("admin","store");
+                    auth.requestMatchers(HttpMethod.POST, "/product/**").hasAnyRole("admin","store");
+                    auth.requestMatchers(HttpMethod.PUT, "/product/**").hasAnyRole("admin","store");
+                    auth.requestMatchers(HttpMethod.DELETE, "/product/**").hasAnyRole("admin","store");
 
                     auth.requestMatchers(HttpMethod.GET,"/categories/**").permitAll();
-                    auth.requestMatchers(HttpMethod.POST, "/categories/**").hasAnyAuthority("admin");
-                    auth.requestMatchers(HttpMethod.PUT, "/categories/**").hasAnyAuthority("admin");
-                    auth.requestMatchers(HttpMethod.DELETE, "/categories/**").hasAnyAuthority("admin");
+                    auth.requestMatchers(HttpMethod.POST, "/categories/**").hasRole("admin");
+                    auth.requestMatchers(HttpMethod.PUT, "/categories/**").hasRole("admin");
+                    auth.requestMatchers(HttpMethod.DELETE, "/categories/**").hasRole("admin");
 
                     auth.requestMatchers(HttpMethod.GET,"/roles/**").permitAll();
-                    auth.requestMatchers(HttpMethod.POST, "/roles/**").hasAnyAuthority("admin");
-                    auth.requestMatchers(HttpMethod.PUT, "/roles/**").hasAnyAuthority("admin");
-                    auth.requestMatchers(HttpMethod.DELETE, "/roles/**").hasAnyAuthority("admin");
+                    auth.requestMatchers(HttpMethod.POST, "/roles/**").hasRole("admin");
+                    auth.requestMatchers(HttpMethod.PUT, "/roles/**").hasRole("admin");
+                    auth.requestMatchers(HttpMethod.DELETE, "/roles/**").hasRole("admin");
 
-                    auth.requestMatchers("/address/**").permitAll();
+                    auth.requestMatchers(HttpMethod.GET,"/address/**").hasAnyRole("admin","store","customer");
 
                     auth.requestMatchers("/signup/**").permitAll();
                     auth.requestMatchers("/login/**").permitAll();
@@ -70,7 +115,9 @@ public class SecurityConfig {
                     auth.requestMatchers("/user/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .build();
     }
 }
