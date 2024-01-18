e96d431eb9b34b6c4f73db3f7d4c67e990978d52
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-19 01:20:39 +0300

    created payment entity

---
+++ src/main/java/org/workintech/entity/user/Payment.java
@@ -0,0 +1,35 @@
+package org.workintech.entity.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "payment",schema = "ecommerce")
+public class Payment {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "expiry_month")
+    private String expiryMonth;
+
+    @Column(name = "expiry_year")
+    private String expiryYear;
+
+    @Column(name = "cvc")
+    private String cvc;
+
+
+}
