96e16278b5c68d3ea1a3943beb81b487d4f1d7d4
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-01-19 01:21:48 +0300

    created payment controller

---
+++ src/main/java/org/workintech/controller/user/PaymentController.java
@@ -0,0 +1,29 @@
+package org.workintech.controller.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.workintech.dto.user.PaymentResponse;
+import org.workintech.entity.user.Payment;
+import org.workintech.service.user.PaymentService;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    private PaymentService paymentService;
+    @PostMapping("/{token}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaymentResponse save(@NotNull @RequestBody Payment payment, @NotBlank @PathVariable String token){
+        return paymentService.save(payment,token);
+    }
+
+    @GetMapping("/{token}")
+    public List<PaymentResponse> getAll(@NotBlank @PathVariable String token){
+        return paymentService.getByUserToken(token);
+    }
+}
