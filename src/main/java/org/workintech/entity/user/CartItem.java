24f34ee98d7fc2a25795a13e4206080ebb0a58f8
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-02-04 00:28:05 +0300

    created CartItem entity layer

---
+++ src/main/java/org/workintech/entity/user/CartItem.java
@@ -0,0 +1,32 @@
+package org.workintech.entity.user;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.workintech.entity.Product;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "cart_item", schema = "ecommerce")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "is_checked")
+    private Boolean isChecked;
+}
