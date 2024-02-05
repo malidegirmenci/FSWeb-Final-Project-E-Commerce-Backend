a39965ee8baeb30cdf8697c9fce040eb2b980ad8
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2024-02-05 03:41:50 +0300

    added id

--- src/main/java/org/workintech/dto/user/AddressResponse.java
+++ src/main/java/org/workintech/dto/user/AddressResponse.java
@@ -1,4 +1,4 @@
 package org.workintech.dto.user;
 
-public record AddressResponse(String addressTitle, String name, String surname , String phone, String city, String district, String neighborhood, String addressDetail) {
+public record AddressResponse(Long id, String addressTitle, String name, String surname , String phone, String city, String district, String neighborhood, String addressDetail) {
 }
