73796b8ca0d6e9002a5085fa26d297ad1fc95caa
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2024-02-14 20:13:49 +0300

    changed saveOrder method's return value as order response

--- src/main/java/org/workintech/controller/user/OrderController.java
+++ src/main/java/org/workintech/controller/user/OrderController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.workintech.dto.user.OrderRequest;
 import org.workintech.dto.user.OrderResponse;
+import org.workintech.service.user.CartItemService;
 import org.workintech.service.user.OrderService;
 
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.List;
 public class OrderController {
     private OrderService orderService;
     @PostMapping("/{token}")
-    private String saveOrder(@PathVariable String token, @RequestBody OrderRequest orderRequest){
+    private OrderResponse saveOrder(@PathVariable String token, @RequestBody OrderRequest orderRequest){
         return orderService.saveOrder(token,orderRequest);
     }
 
